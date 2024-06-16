ff6d98954ad25700abf730c332673e178d124659
r-kundan <renkundan@gmail.com>
2024-06-16 15:49:48 +0530

    Add MostPopularFood component and roundedButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { SecondaryHeading, PrimaryHeading } from "@/components/Heading";
+import MostPopularFood from "@/components/MostPopularFood"
 
 export default function Home() {
   return (
     <main>
       <PrimaryHeading>Home</PrimaryHeading>
       <SecondaryHeading>Home</SecondaryHeading>
-      Poppin
+      
+      <MostPopularFood/>
     </main>
   );
 }
