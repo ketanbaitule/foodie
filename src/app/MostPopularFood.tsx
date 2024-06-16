89646011ee38077184bfb57f9951486e273a0e54
r-kundan <renkundan@gmail.com>
2024-06-16 17:35:40 +0530

    Update MostPopularFood

--- src/app/MostPopularFood.tsx
+++ src/app/MostPopularFood.tsx
@@ -11,7 +11,7 @@ function MostPopularFood() {
         <RoundedButton text={"See more"} />
       </div>
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
            <ProductItem foodName={"Sweet Corn Soup"} discount ={"Flat Rs.100 off"} price={"119"} rating={"5.0"} imgUrl={"/food.jpg"}/>
       <ProductItem foodName={"Avocado and Egg Toast"} discount ={"Flat Rs.100 off"} price={"210"} rating={"5.0"} imgUrl={"/food2.avif"}/>
       <ProductItem foodName={"Pancakes for breakfast"} discount ={"Flat Rs.100 off"} price={"200"} rating={"5.0"} imgUrl={"/food3.avif"}/>
