18a80499756c56dc5a2dfd84d06a76ed6afe551c
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-06-16 20:35:40 +0530

    Change name of base -> secondary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,16 +11,10 @@ const config: Config = {
       colors: {
         primary: "#F27704",
         base :"rgba(156 ,163 ,175, 0.5)",
-        textwhite:"#FFFFFF",
         glasscolor: "rgba(255, 255, 255, 0.1)", // Light glassy color
         backdrop: "rgba(0, 0, 0, 0.4)", // Dark backdrop color
+        secondary: "rgba(156 ,163 ,175, 0.5)",
       },
-    },
-    colors: {
-      "primary": "#F27705",
-      glasscolor: "rgba(255, 255, 255, 0.1)", // Light glassy color
-        backdrop: "rgba(0, 0, 0, 0.4)", // Dark backdrop color
-       
     },
   },
   plugins: [],
