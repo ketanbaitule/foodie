931ebeb33b62efb65b21a719f89ab4955c2ae4ab
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-06-16 20:37:07 +0530

    Refractor ProductItem

--- src/app/MostPopularFood.tsx
+++ src/app/MostPopularFood.tsx
@@ -1,24 +1,46 @@
-import React from 'react'
+import React from "react";
 import { SecondaryHeading } from "@/components/Heading";
 import RoundedButton from "@/components/RoundedButton";
-import ProductItem from "@/components/ProductItem"
+import ProductItem from "@/components/ProductItem";
 
 function MostPopularFood() {
+  const popularFoodItems = [
+    {
+      foodName: "Sweet Corn Soup",
+      discount: "Flat Rs.100 off",
+      price: "119 for two",
+      rating: "5.0",
+      imgUrl: "/food.jpg",
+    },
+    {
+      foodName: "Avocado and Egg Toast",
+      discount: "Flat Rs.100 off",
+      price: "210 for two",
+      rating: "5.0",
+      imgUrl: "/food2.avif",
+    },
+    {
+      foodName: "Pancakes for breakfast",
+      discount: "Flat Rs.100 off",
+      price: "200 for two",
+      rating: "5.0",
+      imgUrl: "/food3.avif",
+    },
+  ];
   return (
     <>
-       <div className="flex justify-between">
+      <div className="flex justify-between">
         <SecondaryHeading>Most Popular Food</SecondaryHeading>
         <RoundedButton text={"See more"} />
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-           <ProductItem foodName={"Sweet Corn Soup"} discount ={"Flat Rs.100 off"} price={"119"} rating={"5.0"} imgUrl={"/food.jpg"}/>
-      <ProductItem foodName={"Avocado and Egg Toast"} discount ={"Flat Rs.100 off"} price={"210"} rating={"5.0"} imgUrl={"/food2.avif"}/>
-      <ProductItem foodName={"Pancakes for breakfast"} discount ={"Flat Rs.100 off"} price={"200"} rating={"5.0"} imgUrl={"/food3.avif"}/>
-    
-       </div> 
+      <div className="flex flex-row flex-wrap justify-evenly gap-4">
+        {popularFoodItems.map((item, index) => (
+          <ProductItem key={index} item={item} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default MostPopularFood
+export default MostPopularFood;
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,34 +1,30 @@
-import React from "react";
+import Image from "next/image";
+import { BiSolidStar } from "react-icons/bi";
 
-function ProductItem({foodName,discount,price,rating,imgUrl}) {
+function ProductItem({ item }) {
   return (
     <>
-      <div className="flex justify-evenly items-center flex-wrap">
-        <div className="w-80 h-[461px]">
-          <div className="h-44 w-44 rounded-full relative top-20 left-4 bg-slate-200">
-            <img
-              src={imgUrl}
-              className="h-44 w-44 rounded-full"
-              alt="img product"
-            />
+      <div className="w-72 -mt-12">
+        <div className="h-36 w-36 rounded-full relative top-20 left-4 bg-slate-200">
+          <Image
+            src={item.imgUrl}
+            className="rounded-full"
+            fill
+            alt="img product"
+          />
+        </div>
+        <div className="rounded-3xl bg-secondary flex flex-col items-center p-5 min-h-80">
+          <div className="self-end justify-self-start text-lg ">
+            {item.rating}
+            <BiSolidStar className="ml-2 fill-primary w-6 h-6 inline" />
           </div>
-          <div className="w-[305.5px] h-[350px] rounded-[50px] bg-base flex flex-col items-center p-5">
-            <div className="flex justify-center items-center gap-1 self-end">
-              <h1 className="text-lg">{rating}</h1>
-              <svg
-                className="fill-primary w-6 h-6"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 576 512"
-              >
-                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-              </svg>
-            </div>
 
-            <div className="flex flex-col justify-center items-center pt-16 gap-5">
-              <h2 className="text-3xl text-wrap">{foodName}</h2>
-              <h5 className="text-2xl">{discount}</h5>
-              <h6 className="text-3xl">&#8377;{price}</h6>
-            </div>
+          <div className="flex flex-col grow justify-center items-center gap-5">
+            <h2 className="text-wrap text-center font-medium text-2xl">
+              {item.foodName}
+            </h2>
+            <h5 className="font-medium text-xl">{item.discount}</h5>
+            <h6 className="text-2xl font-medium "> ₹ {item.price}</h6>
           </div>
         </div>
       </div>
