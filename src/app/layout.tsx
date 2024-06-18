6be57db2bbeaf2a4eeb199ad45e1b37ba1764f7c
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-06-18 13:40:18 +0000

    Add Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Navbar";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -19,10 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.className} bg-[#121212] text-[#F7F7F7] px-4 md:px-8 container mx-auto`}
-      >
-        {children}
+      <body className={`${poppins.className} bg-[#121212] text-[#F7F7F7]`}>
+        <Navbar />
+        <div className="px-4 md:px-8 container mx-auto">
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,7 @@ import MostPopularFood from "./MostPopularFood";
 export default function Home() {
   return (
     <main>
-      <PrimaryHeading>Home</PrimaryHeading>
-
       <SecondaryHeading>Categories</SecondaryHeading>
-
       <CategoryBox variant={"default"} />
       <About />
       <MostPopularFood />
