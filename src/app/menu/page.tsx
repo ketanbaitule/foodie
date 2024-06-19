659673e991ee8aa9070ebe24fba49697d9827c63
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-06-19 09:51:17 +0000

    Menu fixes

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,13 +1,15 @@
 import CategoryBox from "@/components/Categories/CategoryBox";
 import TempBox from "@/components/TempBox";
 import Menus from "@/app/menu/Menus";
+import { SecondaryHeading } from "@/components/Heading";
 
 
 
 export default function Menu() {
   return (
-    <div className="flex flex-col gap-y-5 my-5">
-    <Menus/>
+    <div className="flex flex-col gap-y-8 my-5">
+      <Menus/>
+      <SecondaryHeading>Categories</SecondaryHeading>
       <CategoryBox variant={"circle"} />
       <TempBox name="Our Specials" />
       <TempBox name="Desserts" />
