e1844ccda37a3b66745b32651d84cca33cbc8a46
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-06-21 11:41:26 +0530

    Complete About page

---
+++ src/app/about/MeetTheTeam.tsx
@@ -0,0 +1,50 @@
+import { SecondaryHeading } from "@/components/Heading";
+import Image from "next/image";
+
+function TeamMember({ name, role, image }) {
+  return (
+    <div className="flex flex-col items-stretch">
+      <Image src={image} alt={name} width={250} height={300} />
+      <div className="bg-secondary text-center p-4 rounded-b-xl">
+        <h3 className="text-xl font-semibold">{name}</h3>
+      </div>
+    </div>
+  );
+}
+
+function MeetTheTeam() {
+  return (
+    <div className="flex flex-col items-center gap-y-5">
+      <SecondaryHeading>Meet The Team</SecondaryHeading>
+      <p className="text-center text-xl md:text-2xl pl-2">
+        Our talented team of chefs and food enthusiasts are the backbone of
+        Foodie. <br /> Meet the people who bring our delicious dishes to life
+        every day.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center w-full gap-10">
+        <TeamMember
+          name="John Doe"
+          role="Head Chef"
+          image="/about/team/team1.png"
+        />
+        <TeamMember
+          name="John Doe"
+          role="Head Chef"
+          image="/about/team/team2.png"
+        />
+        <TeamMember
+          name="John Doe"
+          role="Head Chef"
+          image="/about/team/team3.png"
+        />
+        <TeamMember
+          name="John Doe"
+          role="Head Chef"
+          image="/about/team/team4.png"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MeetTheTeam;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import { PrimaryHeading, SecondaryHeading } from "@/components/Heading";
+import OurPhilosophy from "./OurPhilosophy";
+import MeetTheTeam from "./MeetTheTeam";
+import Gallary from "./Gallary";
 
 export default function About() {
-    return (
-      <div className="flex flex-col justify-center items-center gap-8">
+  return (
+    <div className="flex flex-col justify-center items-center gap-8">
       <PrimaryHeading>About Us</PrimaryHeading>
       <div className="flex flex-col justify-center items-center gap-10">
         <p className="text-center text-lg md:text-2xl">
@@ -15,12 +18,15 @@ export default function About() {
         <div className="grid grid-cols-11 gap-3 max-sm:gap-2 px-5">
           {about.map((src, index) => {
             return (
-              <div key={index} className={`w-14 h-72 max-lg:w-9 max-sm:w-7 max-[480px]:w-6 relative col-span-1 shadow-[0_4px_16px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)] shadow-slate-500 ${index===0 ? 'rounded-l-xl':''} ${index===about.length-1 ? 'rounded-r-xl':''}`}>
+              <div
+                key={index}
+                className={`w-14 h-72 max-lg:w-9 max-sm:w-7 max-[480px]:w-6 relative col-span-1 shadow-[0_4px_16px_-1px_rgb(0,0,0,0.1),0_2px_4px_-2px_rgb(0,0,0,0.1)] shadow-slate-500 ${index === 0 ? "rounded-l-xl" : ""} ${index === about.length - 1 ? "rounded-r-xl" : ""}`}
+              >
                 <Image
                   src={src}
                   alt={`Food Image ${index + 1} `}
                   layout="fill"
-                  className={`object-cover object-center w-full ${index===0 ? 'rounded-l-xl':''} ${index===about.length-1 ? 'rounded-r-xl':''}`}
+                  className={`object-cover object-center w-full ${index === 0 ? "rounded-l-xl" : ""} ${index === about.length - 1 ? "rounded-r-xl" : ""}`}
                 />
               </div>
             );
@@ -28,23 +34,31 @@ export default function About() {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row items-center justify-around w-full mt-5 p-10">
-        <div className="relative flex-1">
-          <Image src="/about-story.png" alt="about-story-image" objectFit="cover" width={400} height={300}/>
+      <div className="flex flex-col md:flex-row items-center justify-around w-full mt-5 p-10 gap-y-5">
+        <div className="relative flex-1 max-md:order-2">
+          <Image
+            src="/about-story.png"
+            alt="about-story-image"
+            objectFit="cover"
+            width={400}
+            height={300}
+          />
         </div>
-        <div className="flex flex-1 flex-col items-start gap-5 md:ml-10 mt-8">
+        <div className="flex flex-1 flex-col items-start gap-5 md:ml-10 mt-8 max-md:order-1">
           <SecondaryHeading>Our Story</SecondaryHeading>
           <p className="text-xl md:text-2xl pl-2">
-            Founded in  2023, Foodie began as a
-            small family-owned seafood restaurant.
-            Over the years, we&apos;ve expanded our menu
-            and our reach, always maintaining our
-            commitment to quality and flavor.
+            Founded in 2023, Foodie began as a small family-owned seafood
+            restaurant. Over the years, we&apos;ve expanded our menu and our
+            reach, always maintaining our commitment to quality and flavor.
           </p>
         </div>
       </div>
+
+      <OurPhilosophy />
+      <MeetTheTeam />
+      <Gallary />
     </div>
-    );
+  );
 }
 
 const about = [
@@ -59,4 +73,4 @@ const about = [
   "/about9.png",
   "/about10.png",
   "/about11.png",
-]
+];
